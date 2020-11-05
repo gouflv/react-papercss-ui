350ea5e2ab400c336680005d48ae76e5867f25fc
gouflv <lv.gouf@gmail.com>
2020-11-05 18:21:11 +0800

    feat: initial Collapse component

---
+++ components/collapse/Collapse.stories.tsx
@@ -0,0 +1,43 @@
+import { Story } from '@storybook/react/types-6-0'
+import React from 'react'
+import { Collapse, CollapseProps } from './Collapse'
+
+const { Panel } = Collapse
+
+export default {
+  title: 'Collapse',
+  component: Collapse
+}
+
+const Template: Story<CollapseProps> = args => <Collapse {...args} />
+
+const LOREM =
+  'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam beatae commodi enim labore neque officia suscipit velit vero. Assumenda, consequuntur, cupiditate. A, animi beatae dolorem ducimus et exercitationem iste quidem.'
+
+const BasicPanels = [
+  <Panel key='1' name='1' header='Fist Panel'>
+    {LOREM}
+  </Panel>,
+  <Panel key='2' name='2' header='Second Panel'>
+    {LOREM}
+  </Panel>,
+  <Panel key='3' name='3' header='Third Panel'>
+    {LOREM}
+  </Panel>
+]
+
+export const Basic = Template.bind({})
+Basic.args = { activeKey: ['1', '3'], children: BasicPanels } as CollapseProps
+
+export const Accordion = Template.bind({})
+Accordion.args = { accordion: true, children: BasicPanels } as CollapseProps
+
+export const Nesting = Template.bind({})
+Nesting.args = {
+  activeKey: '1',
+  children: [
+    <Panel key='1' name='1' header='Parent Panel'>
+      <Collapse>{BasicPanels}</Collapse>
+    </Panel>
+  ]
+}
---
+++ components/collapse/Collapse.tsx
@@ -0,0 +1,88 @@
+import React, {
+  FC,
+  PropsWithChildren,
+  ReactElement,
+  ReactNode,
+  useEffect,
+  useState
+} from 'react'
+
+export type CollapseProps = PropsWithChildren<{
+  activeKey?: string | string[] | number | number[]
+  accordion?: boolean
+}>
+
+function toArray<T>(children: ReactNode) {
+  return React.Children.toArray(children) as T[]
+}
+
+const Collapse = (props: PropsWithChildren<CollapseProps>) => {
+  const [active, setActive] = useState<string[]>([])
+
+  useEffect(() => {
+    if (!props.activeKey) return
+    if (Array.isArray(props.activeKey)) {
+      setActive((props.activeKey as any[]).map(k => String(k)))
+    } else {
+      setActive([String(props.activeKey)])
+    }
+  }, [props.activeKey])
+
+  function childRender() {
+    return toArray<ReactElement>(props.children).map((child, index) => {
+      const key = String(child.props.name) || String(index)
+      const isActive = !!~active.indexOf(key)
+      return React.cloneElement(child, {
+        ...child.props,
+        key,
+        active: isActive,
+        onClick: onPanelClick
+      } as PanelProps)
+    })
+  }
+
+  function onPanelClick(key: string | number) {
+    if (props.accordion) {
+      setActive([String(key)])
+    } else {
+      setActive(prevState => {
+        const keySet = new Set(prevState)
+        keySet.add(String(key))
+        return Array.from(keySet)
+      })
+    }
+  }
+
+  return <div className='collapse-group'>{childRender()}</div>
+}
+
+export interface PanelProps {
+  name?: number | string
+  header: ReactNode
+  active?: boolean
+  onClick?: (key: string | number) => void
+}
+
+const Panel: FC<PanelProps> = props => {
+  return (
+    <div className='collapsible'>
+      <label
+        onClick={() => props.onClick && props.name && props.onClick(props.name)}
+      >
+        {props.header}
+      </label>
+      {!!props.active && (
+        <div
+          className='collapsible-body'
+          style={{ maxHeight: 'none', padding: '0.75rem', opacity: 1 }}
+        >
+          {props.children}
+        </div>
+      )}
+    </div>
+  )
+}
+
+Collapse.Panel = Panel
+
+export { Collapse }
